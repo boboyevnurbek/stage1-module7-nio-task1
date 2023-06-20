bd959247c7a0c1943a5999cbc7dd39b12541b90c
boboyevnurbek <boboyevnurbek95@gmail.com>
2023-06-20 11:56:28 +0500

    NIO TASK solution-1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,10 +1,8 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.stream.Stream;
 
 
