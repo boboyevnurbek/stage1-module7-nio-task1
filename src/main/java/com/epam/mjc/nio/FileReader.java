cd54523165be7a9cd2b530122b319664a7a68929
boboyevnurbek <boboyevnurbek95@gmail.com>
2023-06-20 11:55:56 +0500

    NIO TASK

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,34 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        Profile profile = new Profile();
+        Path path = Path.of(file.getPath());
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(line -> {
+                if (line.startsWith("Name: ")) {
+                    profile.setName(line.substring(6));
+                } else if (line.startsWith("Age: ")) {
+                    profile.setAge(Integer.parseInt(line.substring(5)));
+                } else if (line.startsWith("Email: ")) {
+                    profile.setEmail(line.substring(7));
+                } else if (line.startsWith("Phone: ")) {
+                    profile.setPhone(Long.parseLong(line.substring(7)));
+                }
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
